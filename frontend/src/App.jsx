69044847b76c6332b8cf4c3f97cf643857aeb642
Luis Martinez <38267717+Lmx154@users.noreply.github.com>
2024-10-10 13:50:03 -0500

    init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react'
+
+
+function App() {
+  const [data, setData] = useState([])
+
+  useEffect(()=> {
+    async function fetchData() {
+      console.log(import.meta.env.VITE_API_URL)
+      try {
+        const response = await fetch(`${import.meta.env.VITE_API_URL}`)
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const result = await response.json();
+        console.log(result)
+        setData(result);
+      } catch(error) {
+        console.error('Error fetching data:', error);
+      }
+
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      hello world
+    </>
+  )
+}
+
+export default App
